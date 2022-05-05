10709329ac9199fdfa5377dfbbb083503c07eab9
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2022-05-05 15:29:10 -0400

    Java 17

--- social-click/src/main/java/com/example/SocialClickApplication.java
+++ social-click/src/main/java/com/example/SocialClickApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class SocialClickApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SocialClickApplication.class, args);
 	}
-
 }
